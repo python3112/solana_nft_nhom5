2b17497d66001f7f774d2a89918f27d170bbe0ca
python3112 <hoangporon3112@gmail.com>
2024-03-21 13:45:01 +0700

    all ui

--- src/screens/BlankScreen.tsx
+++ src/screens/BlankScreen.tsx
@@ -1,18 +1,56 @@
-import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
-export default function BlankScreen() {
-  return (
-    <>
-      <View style={styles.screenContainer}>
-        <Text variant="titleLarge">This is a blank tab!</Text>
-      </View>
-    </>
-  );
+import React from 'react';
+import { View, Text, SafeAreaView, FlatList } from 'react-native';
+
+interface ItemProps {
+  name: string;
+  score: number;
 }
 
-const styles = StyleSheet.create({
-  screenContainer: {
-    height: "100%",
-    padding: 16,
-  },
+const dummyData = [
+  { id: 1, name: 'Nguyen van linh', score: 0.5 },
+  { id: 2, name: 'Nguyen van linh', score: 0.5 },
+  { id: 3, name: 'Nguyen van linh', score: 0.5 },
+  { id: 4, name: 'Nguyen van linh', score: 0.5 },
+  { id: 5, name: 'Nguyen van linh', score: 0.5 },
+];
+
+const ItemView: React.FC<ItemProps> = React.memo(({ name, score }) => {
+  return (
+    <View
+      style={{
+        width: '95%',
+        backgroundColor: 'white',
+        marginVertical: 10,
+        shadowColor: 'black',
+        shadowOffset: { height: 1, width: 1 },
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        borderWidth: 0.5,
+        borderColor: 'silver',
+        borderRadius: 20,
+        alignSelf: 'center',
+        padding: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+      }}>
+      <Text style={{ color: 'black', fontSize: 15 }}>{name}</Text>
+      <Text style={{ color: 'black', fontSize: 15 }}>{score}</Text>
+    </View>
+  );
 });
+
+const BlankScreen = () => {
+  return (
+    <SafeAreaView style={{ width: '90%', height: '100%', alignSelf: 'center' }}>
+      <Text style={{ alignSelf: 'center', fontSize: 25, marginBottom: 20, fontWeight: "bold" }}>Bảng xếp hạng</Text>
+      <FlatList
+        data={dummyData}
+        keyExtractor={e => e.id.toString()}
+        renderItem={({ item, index }) => <ItemView name={item.name} score={item.score} />}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default BlankScreen;
--- src/screens/TransicionHitoryScreen.tsx
+++ src/screens/TransicionHitoryScreen.tsx
@@ -1,16 +1,16 @@
 import { format } from 'date-fns';
-import { FlatList, StyleSheet, View, Text } from "react-native";
+import { FlatList, StyleSheet, View, Text, TouchableOpacity, Button } from "react-native";
 import { useEffect, useState } from 'react'
 import { ProgressBar } from "react-native-paper";
 import { useAuthorization } from "../utils/useAuthorization";
 import { ProgressBarTsx } from "../utils/ProgressBar";
-import { ShyftSdk, Network } from '@shyft-to/js'
-import { ParsedTransactionWithMeta } from "@solana/web3.js";
+import { ShyftSdk, Network } from '@shyft-to/js';
+
 
 interface SimplifiedTxnInfo {
     fee_payer: string;
-    amount:number;
-    fee : number;
+    amount: number;
+    fee: number;
     timestamp: string;
 }
 
@@ -19,6 +19,7 @@ export default function TransicionHitoryScreen() {
     const [listTransactions, setlistTransactions] = useState<SimplifiedTxnInfo[]>([]);
 
     useEffect(() => {
+        console.log('vào màn history')
         const fetchData = async () => {
             if (selectedAccount) {
                 const shyft = new ShyftSdk({ apiKey: "M1S8rBLftYC6xhMu", network: Network.Devnet });
@@ -28,46 +29,42 @@ export default function TransicionHitoryScreen() {
                     enableRaw: true,
                 });
 
-              transactions.forEach(element => {
-                element.actions.map(info =>({
-                
-                   
-                    
-                }))
-              });
+             console.log(transactions);
 
-              const simplifiedTransactions: SimplifiedTxnInfo[] = transactions.map(transaction => {
-                const amount = transaction.actions
-                    .map(action => action.info.amount)
-                    
-                return {
-                    fee_payer: transaction.fee_payer,
-                    amount: amount.at(2),
-                    fee: transaction.fee,
-                    timestamp: format(new Date(transaction.timestamp), 'dd/MM/yyyy HH:mm:ss'),
-                    type: transaction.type
-                };
-            });
+
+                const simplifiedTransactions: SimplifiedTxnInfo[] = transactions.map(transaction => {
+                  
+                    const amount = transaction.actions.map(action => action.info.amount)
+                  
+                    return {
+                        fee_payer: transaction.fee_payer,
+                        amount: transaction.actions.length == 1  ? transaction.actions[0].info.amount : amount.at(2),
+                        fee: transaction.fee,
+                        timestamp: format(new Date(transaction.timestamp), 'dd/MM/yyyy HH:mm:ss'),
+                        type: transaction.type
+                    };
+                });
                 setlistTransactions(simplifiedTransactions);
             }
         };
+       
         fetchData();
     }, [selectedAccount]);
 
-    const renderItem = ({ item }: { item: SimplifiedTxnInfo }) => 
-        (
-            <View style={item.fee_payer === selectedAccount?.publicKey.toString() ? styles.item2 :  styles.item }>
-                <Text style={{fontSize : 16 , color : '#FFFFFF' , marginStart: 5}}>Amount: {item.amount} Sol</Text>
-                <Text style={{fontSize : 16 , color : '#FFFFFF' , marginStart: 5}}>Fee: {item.fee} Sol</Text>
-                <Text style={{fontSize : 10 , color : '#FFFFFF' , marginStart: 5}}>Time: {item.timestamp}</Text>
-            </View>  
-       )
+    const renderItem = ({ item }: { item: SimplifiedTxnInfo }) =>
+    (
+        <View style={item.fee_payer === selectedAccount?.publicKey.toString() ? styles.item2 : styles.item} >
+            <Text style={{ fontSize: 16, color: '#FFFFFF', marginStart: 5 }}>Amount: {item.amount == null ? 1 : item.amount} Sol</Text>
+            <Text style={{ fontSize: 16, color: '#FFFFFF', marginStart: 5 }}>Fee: {item.fee} Sol</Text>
+            <Text style={{ fontSize: 10, color: '#FFFFFF', marginStart: 5 }}>Time: {item.timestamp}</Text>
+        </View>
+    )
 
-  
+
 
     return (
         <View style={styles.screenContainer}>
-            <Text style={{ fontWeight: "bold", marginBottom: 12, textAlign: 'center',  fontSize :25 , fontStyle:'normal'}}>
+            <Text style={{ fontWeight: "bold", marginBottom: 12, textAlign: 'center', fontSize: 25, fontStyle: 'normal' }}>
                 Transactions History
             </Text>
             {selectedAccount ? (
@@ -89,24 +86,24 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     item: {
-    
+
         backgroundColor: '#32CD32',
-       paddingTop:10,
-        paddingBottom:10,
-        paddingEnd:5,
-        paddingStart:5,
-        marginTop:10,
-        borderRadius:10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingEnd: 5,
+        paddingStart: 5,
+        marginTop: 10,
+        borderRadius: 10,
     },
     item2: {
-       
+
         backgroundColor: '#FA8072',
-        paddingTop:10,
-        paddingBottom:10,
-        paddingEnd:7,
-        paddingStart:7,
-        marginTop:10,
-    
-        borderRadius:10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingEnd: 7,
+        paddingStart: 7,
+        marginTop: 10,
+
+        borderRadius: 10,
     },
 });
